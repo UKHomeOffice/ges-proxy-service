a45e34ff928dace5155b18b503e02a3ca82295b3
David Ainslie <dainslie@gmail.com>
2015-10-07 22:16:45 +0100

    Refactor

--- src/main/scala/uk/gov/homeoffice/ges/proxy/Boot.scala
+++ src/main/scala/uk/gov/homeoffice/ges/proxy/Boot.scala
@@ -6,7 +6,8 @@ import grizzled.slf4j.Logging
 import uk.gov.homeoffice.configuration.HasConfig
 import uk.gov.homeoffice.console.Console
 import uk.gov.homeoffice.rtp.proxy._
-import uk.gov.homeoffice.rtp.proxy.ssl.{SSL, SSLProxyingConfiguration}
+import uk.gov.homeoffice.rtp.proxy.ssl.SSL._
+import uk.gov.homeoffice.rtp.proxy.ssl.SSLProxying
 
 object Boot extends App with HasConfig with Console with Logging {
   present("GES Proxy Service")
@@ -22,23 +23,12 @@ object Boot extends App with HasConfig with Console with Logging {
   }
 
   Try { config.getConfig("ssl") } match {
-    case Success(_) => bootSSL
-    case _ => boot
-  }
+    case Success(_) =>
+      info("Booting as SSL proxy")
+      SSLProxying(sslContext(config)).proxy(proxiedServer, server)
 
-  def bootSSL = {
-    info("Booting as SSL proxy")
-
-    val proxying = new Proxying with SSLProxyingConfiguration {
-      implicit val sslContext = SSL.sslContext(config)
-    }
-
-    proxying.proxy(proxiedServer, server)
-  }
-
-  def boot = {
-    info("Booting proxy")
-    val proxying = new Proxying with ProxyingConfiguration
-    proxying.proxy(proxiedServer, server)
+    case _ =>
+      info("Booting proxy")
+      Proxying().proxy(proxiedServer, server)
   }
 }
