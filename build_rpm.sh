0ff7d377955a5dfd186a34540af9292df4900394
Jay Keshur <jaykeshur.homeoffice@gmail.com>
2015-10-08 14:07:35 +0100

    Add version to build rpm

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+version="1.0.0"
+
 cd fpm
 cp ../target/scala-2.11/ges-proxy-service-assembly*.jar build/usr/share/ges-proxy-service/ges-proxy-service.jar
 fpm -x .git* --config-files etc/ges-proxy-service/application.conf --before-install scripts/preinstall.sh --after-install scripts/postinstall.sh --before-remove scripts/preuninstall.sh --after-remove scripts/postuninstall.sh -C build -t rpm -s dir -d java -n ges-proxy-service -v "$version-$BUILD_NUMBER" -a all .
