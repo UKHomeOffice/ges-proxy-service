9c6511945dd763482e67cfc7e2c9575266b7ab37
David Ainslie <dainslie@gmail.com>
2015-12-23 13:17:02 +0000

    Release 1.1.0 Improved logging

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.0-SNAPSHOT",
+      version := "1.1.0",
       scalaVersion := "2.11.7",
       scalacOptions ++= Seq(
         "-feature",
@@ -69,14 +69,14 @@ object Build extends Build {
 
     proxy.settings(
       libraryDependencies ++= Seq(
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.0-SNAPSHOT" withSources(),
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.0-SNAPSHOT" % Test classifier "tests" withSources(),
-        "uk.gov.homeoffice" %% "rtp-akka-lib" % "1.0-SNAPSHOT" withSources(),
-        "uk.gov.homeoffice" %% "rtp-akka-lib" % "1.0-SNAPSHOT" % Test classifier "tests" withSources() excludeAll ExclusionRule(organization = "org.specs2"),
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0-SNAPSHOT" withSources(),
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0-SNAPSHOT" % Test classifier "tests" withSources(),
-        "uk.gov.homeoffice" %% "rtp-proxy-lib" % "1.0-SNAPSHOT" withSources(),
-        "uk.gov.homeoffice" %% "rtp-proxy-lib" % "1.0-SNAPSHOT" % Test classifier "tests" withSources()
+        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.1.0" withSources(),
+        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.1.0" % Test classifier "tests" withSources(),
+        "uk.gov.homeoffice" %% "rtp-akka-lib" % "1.1.0" withSources(),
+        "uk.gov.homeoffice" %% "rtp-akka-lib" % "1.1.0" % Test classifier "tests" withSources() excludeAll ExclusionRule(organization = "org.specs2"),
+        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0" withSources(),
+        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0" % Test classifier "tests" withSources(),
+        "uk.gov.homeoffice" %% "rtp-proxy-lib" % "1.1.0" withSources(),
+        "uk.gov.homeoffice" %% "rtp-proxy-lib" % "1.1.0" % Test classifier "tests" withSources()
       )
     )
   }
