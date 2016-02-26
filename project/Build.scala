7a0b0c1319205d08ccd0940cf67156e7b4cfba46
David Ainslie <dainslie@gmail.com>
2016-02-26 17:37:56 +0000

    Cleaned up build

--- project/Build.scala
+++ project/Build.scala
@@ -6,10 +6,11 @@ import spray.revolver.RevolverPlugin._
 object Build extends Build {
   val moduleName = "ges-proxy-service"
 
-  lazy val proxy = Project(id = moduleName, base = file(".")).enablePlugins(GatlingPlugin)
+  val root = Project(id = moduleName, base = file(".")).enablePlugins(GatlingPlugin)
     .configs(IntegrationTest)
     .settings(Revolver.settings)
     .settings(Defaults.itSettings: _*)
+    .settings(run := (run in Runtime).evaluated) // Required to stop Gatling plugin overriding the default "run".
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
@@ -30,54 +31,32 @@ object Build extends Build {
         "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
         "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
         "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases",
-        "Kamon Repository" at "http://repo.kamon.io"),
-      libraryDependencies ++= Seq(
-        "com.typesafe.akka" %% "akka-slf4j" % "2.4.0" withSources()
-      ),
-      libraryDependencies ++= Seq(
-        "com.typesafe.akka" %% "akka-testkit" % "2.3.12" % "test, it" withSources(),
+        "Kamon Repository" at "http://repo.kamon.io"
+      )
+    )
+    .settings(libraryDependencies ++= {
+      val `akka-version` = "2.4.0"
+      val `gatling-version` = "2.1.7"
+      val `rtp-io-lib-version` = "1.2.0-SNAPSHOT"
+      val `rtp-test-lib-version` = "1.2.0-SNAPSHOT"
+      val `rtp-akka-lib-version` = "1.5.0-SNAPSHOT"
+      val `rtp-proxy-lib-version` = "1.2.0-SNAPSHOT"
+
+      Seq(
+        "com.typesafe.akka" %% "akka-slf4j" % `akka-version` withSources(),
+        "uk.gov.homeoffice" %% "rtp-io-lib" % `rtp-io-lib-version` withSources(),
+        "uk.gov.homeoffice" %% "rtp-test-lib" % `rtp-test-lib-version` withSources(),
+        "uk.gov.homeoffice" %% "rtp-akka-lib" % `rtp-akka-lib-version` withSources(),
+        "uk.gov.homeoffice" %% "rtp-proxy-lib" % "1.2.0-SNAPSHOT" withSources()
+      ) ++ Seq(
+        "com.typesafe.akka" %% "akka-testkit" % `akka-version` % "test, it" withSources(),
         "io.spray" %% "spray-testkit" % "1.3.3" % "test, it" withSources() excludeAll ExclusionRule(organization = "org.specs2"),
-        "io.gatling.highcharts" % "gatling-charts-highcharts" % "2.1.7" % IntegrationTest withSources(),
-        "io.gatling" % "gatling-test-framework" % "2.1.7" % IntegrationTest withSources()
+        "io.gatling.highcharts" % "gatling-charts-highcharts" % `gatling-version` % IntegrationTest withSources(),
+        "io.gatling" % "gatling-test-framework" % `gatling-version` % IntegrationTest withSources(),
+        "uk.gov.homeoffice" %% "rtp-io-lib" % `rtp-io-lib-version` % Test classifier "tests" withSources(),
+        "uk.gov.homeoffice" %% "rtp-test-lib" % `rtp-test-lib-version` % Test classifier "tests" withSources(),
+        "uk.gov.homeoffice" %% "rtp-akka-lib" % `rtp-akka-lib-version` % Test classifier "tests" withSources() excludeAll ExclusionRule(organization = "org.specs2"),
+        "uk.gov.homeoffice" %% "rtp-proxy-lib" % `rtp-proxy-lib-version` % Test classifier "tests" withSources()
       )
-    )
-    .settings(run := (run in Runtime).evaluated) // Required to stop Gatling plugin overriding the default "run".
-
-  val ioPath = "../rtp-io-lib"
-  val akkaPath = "../rtp-akka-lib"
-  val testPath = "../rtp-test-lib"
-  val rtpProxyPath = "../rtp-proxy-lib"
-
-  val root = if (file(ioPath).exists && sys.props.get("jenkins").isEmpty) {
-    println("=============")
-    println("Build Locally")
-    println("=============")
-
-    val io = ProjectRef(file(ioPath), "rtp-io-lib")
-    val akka = ProjectRef(file(akkaPath), "rtp-akka-lib")
-    val test = ProjectRef(file(testPath), "rtp-test-lib")
-    val rtpProxy = ProjectRef(file(rtpProxyPath), "rtp-proxy-lib")
-
-    proxy.dependsOn(io % "test->test;compile->compile")
-         .dependsOn(akka % "test->test;compile->compile")
-         .dependsOn(test % "test->test;compile->compile")
-         .dependsOn(rtpProxy % "test->test;compile->compile")
-  } else {
-    println("================")
-    println("Build on Jenkins")
-    println("================")
-
-    proxy.settings(
-      libraryDependencies ++= Seq(
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.2.0-SNAPSHOT" withSources(),
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.2.0-SNAPSHOT" % Test classifier "tests" withSources(),
-        "uk.gov.homeoffice" %% "rtp-akka-lib" % "1.5.0-SNAPSHOT" withSources(),
-        "uk.gov.homeoffice" %% "rtp-akka-lib" % "1.5.0-SNAPSHOT" % Test classifier "tests" withSources() excludeAll ExclusionRule(organization = "org.specs2"),
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.0-SNAPSHOT" withSources(),
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.0-SNAPSHOT" % Test classifier "tests" withSources(),
-        "uk.gov.homeoffice" %% "rtp-proxy-lib" % "1.2.0-SNAPSHOT" withSources(),
-        "uk.gov.homeoffice" %% "rtp-proxy-lib" % "1.2.0-SNAPSHOT" % Test classifier "tests" withSources()
-      )
-    )
-  }
+    })
 }
